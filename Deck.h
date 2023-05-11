fafb113530539399057a28f06547e4f7eff774b4
sahwork21 <sahwork21@gmail.com>
2023-05-11 13:19:48 -0500

    Added Deck construction

--- Deck.h
+++ Deck.h
@@ -5,14 +5,89 @@
  * It should also have the capabilities of intializing cards and shuffling the deck.
  * It will also have deal and return cards but these are really just push_back and remove
  * front.
- * 
- * 
+ *
+ *
  */
 #include "Card.h"
-#include <vector>
+#include <deque>
+#include <stdexcept>
+#include <string>
+
+
+//Macros for our names and suits
+#define NAMES {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+#define SUITS {"Clubs", "Spades", "Hearts", "Diamonds"}
 
 class Deck
 {
-  private:
-    std::vector<Card> Cards;
+  // Fields
+private:
+  std::deque<Card> Cards;
+  int count;
+
+public:
+  // Constructors have to differentiate what the value of an ace is depending on our game
+  // Default constructor setting aces to 11 for our concerns of Blackjack
+  Deck()
+  {
+    Deck(11);
+  }
+
+  // Parametrized constructor only allow 11 or 1 for our blackjack game
+  Deck(int aValue)
+  {
+    if (aValue != 1 && aValue != 11)
+    {
+      throw std::invalid_argument("Received invalid Ace value");
+    }
+
+    setCards(aValue);
+    setCount(52);
+  }
+
+  // Destructor where we have to destroy all 52 cards by freeing them
+  ~Deck()
+  {
+    for(int i = 0; i < 52; i++){
+      Card c = Cards.front();
+      Cards.pop_front();
+      free(&c);
+      //Just free all our cards from the deque
+    }
+
+    //Then destroy the deque
+  }
+
+  // Getters and setter
+  std::deque<Card> getCards()
+  {
+    return Cards;
+  }
+
+  void setCards(int aValue)
+  {
+    //We need to construct 52 cards
+    //Construct each of our Cards
+    std::string suits[4] = SUITS;
+    std::string names[13] = NAMES;
+
+
+    //We need to heap allocate these cards so they don't go out of scope when this stack pops off
+    for(int i = 0; i < 13; i++){
+      for(int j = 0; j < 4; j++){
+        Card *c = (Card *) malloc (sizeof(Card));
+        Card *c = new Card(1, names[i], suits[j]);
+        Cards.push_back(*c);
+      }
+    }
+  }
+
+  int getCount()
+  {
+    return count;
+  }
+
+  void setCount(int count){
+    this->count = count;
+  }
 };
