0b76e66c3ca905bead13caed9458c25b129506fa
sahwork21 <sahwork21@gmail.com>
2023-05-23 15:32:32 -0500

    Added shellscript for Deck testing

---
+++ test/DeckTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm *.exe
+
+#Compile the tester with TEST flag set
+g++ -Wall -std=c++17 -g DeckTest.cpp ../src/Deck.cpp ../src/Card.cpp -o DeckTest -DTEST
+
+#Make sure it compiled
+if [ $? -ne 0 ]; then
+    echo "Failed to compile Deck Test"
+    exit 1
+fi
+
+#otherwise we are good
+exit 0
