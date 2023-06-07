214ccd2de155374e31babfe8c2748c3b881c4324
sahwork21 <sahwork21@gmail.com>
2023-06-07 14:05:19 -0500

    Changed Table to use a struct containing the type of player in the game

--- src/Table.cpp
+++ src/Table.cpp
@@ -14,6 +14,30 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+
+//This will be our handles for hitting and returning stuff so we don't have to wait so long
+void hitUser(User user, Card *c)
+{
+  //Just hit depending on what is filling our union
+  if(){
+    user.better->hitCard(c);
+  } 
+}
+
+
+
+//This is the flag to flip off our game
+//It will exit our loop and delete objects
+static volatile sig_atomic_t contFlag = 1;
+/**
+ * On a sig interrupt ctrl C we need to write out our quit message
+ */
+void signalHandle(int signum)
+{
+  contFlag = 0;
+}
+
+
 /**
  * This is the main component that will be responsible for constructing objects and taking bets
  * It will take care of making deck operations and betting.
@@ -21,7 +45,10 @@ using std::endl;
 int main()
 {
 
-  
+  //Keep our wins and hands played
+  int played = 0;
+  int wins = 0;
+
 
   //Ask how many decks we should have in our shoe
   cout << "How many decks should we have in the shoe? Between 1 and 8" << endl;
@@ -55,7 +82,7 @@ int main()
 
 
   //If it's a y make a better. n make a player
-  user player;
+  User player;
   if(option == 'y'){
 
     player.better = new Better();
@@ -66,8 +93,68 @@ int main()
   }
 
 
+  //Shuffle our shoe and place the reshuffle card
+  shoe->washDecks();
+  shoe->setReshuffle();
 
-  //Flush
+  Sleep(3000);
+  //Keep playing until the flag is turned off
+  while(contFlag){
+    cout << "Remember to do ctrl^C to end the game" << endl;
+    Sleep(3000);
+
+
+
+    //Flush input then deal our cards out
+    cin.clear();
+    //Ignore all the characters until an EOF
+    cin.ignore(INT_MAX);
+
+
+
+
+    //Ask for bets if we have a better
+    if(option == 'y'){
+
+      cout << "How much do you want to bet?" << endl;
+      int bet;
+      cin >> bet;
+      bool cont = player.better->makeBet(bet);
+      //Deal our cards to player then dealer
+      Sleep(1000);
+      cout << "Dealing Cards" << endl;
+      player.better->hitCard(shoe->dealCard());
+      player.better->hitCard(shoe->dealCard());
+
+      dealer->hitCard(shoe->dealCard());
+      dealer->hitCard(shoe->dealCard());
+      Sleep(3000);
+      
+      //Tell them what we have
+    }
+    else{
+      //Deal our cards to player then dealer
+      Sleep(1000);
+      cout << "Dealing Cards" << endl;
+      player.player->hitCard(shoe->dealCard());
+      player.player->hitCard(shoe->dealCard());
+
+      dealer->hitCard(shoe->dealCard());
+      dealer->hitCard(shoe->dealCard());
+      Sleep(3000);
+    }
+
+    
+    
+  }
+  
+ 
+
+  //Print out our winnings
+  cout << "You won " << wins << " games of " << played << endl;
+  if(option == 'y'){
+    cout << "You made " << player.better->getMoney() - 500 << " net money" << endl;
+  }
 
 
 
--- src/Table.h
+++ src/Table.h
@@ -9,10 +9,25 @@
 #include "Player.h"
 #include <iostream>
 #include <Windows.h>
+#include <csignal>
 
-//This is the user union that could either be a player or better
-union user
+
+//Enum that detemines what type of user we have
+typedef enum {PlayerType, BetterType} UserType;
+
+
+//Struct that determines if we have better or player
+typedef struct UserStruct
 {
-  Player* player;
-  Better* better;
-};
+  //We have our enum for what type we have
+  UserType type;
+  union
+  {
+    Player* player;
+    Better* better;
+  };
+} User;
+
+//These are a bunch of methods that will do some of the actions for a user
+//Just hit cards and stuff depending on what object is in the union
+void hitUser(User user, Card *c);
